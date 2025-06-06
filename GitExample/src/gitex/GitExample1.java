83887932fb25199df6cd9f5c7f926c9c79474fe3
github kazuha-fujiuchi <kazuha-fujiuchi@kccs.co.jp>
2025-06-06 16:49:17 +0900

    mainブランチから追加

--- GitExample/src/gitex/GitExample1.java
+++ GitExample/src/gitex/GitExample1.java
@@ -5,6 +5,7 @@ public class GitExample1 {
 	public static void main(String[] args) {
 		System.out.println("別ファイルの作成");
 		System.out.println("featureブランチでの作業");
+		System.out.println("mainブランチから追加");
 	}
 
 }
